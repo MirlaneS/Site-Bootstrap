222ef1c5c64832a4a0a06ee05454a479f796f1ca
MirlaneS <112016381+MirlaneS@users.noreply.github.com>
2022-11-29 16:54:22 -0300

    dia 29

--- academia/menu.php
+++ academia/menu.php
@@ -29,9 +29,10 @@
               Loja
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Roupas </a>
-                <a class="dropdown-item" href="#">Suplementos</a>
-                <a class="dropdown-item" href="#">Acessorios</a>
+                <a class="dropdown-item" href="roupas.php">Roupas</a>
+                <a class="dropdown-item" href="suplementos.php">Suplementos</a>
+                <a class="dropdown-item" href="acessorios.php">Acessorios</a>
+                <a class="dropdown-item" href="mochilas.php">Mochilas</a>
                 
               </div>
             </li>
